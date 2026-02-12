cd2f63159df8892f6e05a58fd97572cba721872f
priyalparnami <priyalparnami01@gmail.com>
2026-02-12 12:33:34 +0530

    Add files via upload

---
+++ Q4. (29-01-2026).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+	int a[50],n,odd=0,even=0;
+	printf("Enter number of elements: ");
+	scanf("%d",&n);
+	for (i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		if ((i+1)%2==0)
+		{
+			even+=a[i];
+		}
+		else
+		{
+			odd+=a[i];
+		}
+	}
+	printf("Difference = %d",odd-even);
+}
